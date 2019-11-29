b829555ef5729141f8a0546767f8bfac77f50377
Dian Tanjung <dianmulyanat@gmail.com>
2019-11-29 14:40:16 +0700

    commit pertama

---
+++ src/main/java/id/dian/latihanspring/modulsatu/ModulsatuApplication.java
@@ -0,0 +1,13 @@
+package id.dian.latihanspring.modulsatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulsatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulsatuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/dian/latihanspring/modulsatu/ModulsatuApplicationTests.java
@@ -0,0 +1,13 @@
+package id.dian.latihanspring.modulsatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulsatuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
