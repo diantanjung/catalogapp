0f36942bf40f4cb43d823c833e7e9d2bfd9883f1
Dian Tanjung <dianmulyanat@gmail.com>
2019-11-30 21:57:09 +0700

    add data products

--- src/main/resources/db/migration/V2019102902__Data_Awal.sql
+++ src/main/resources/db/migration/V2019102902__Data_Awal.sql
@@ -1,3 +1,3 @@
 insert into product (id, code, name) values ('p001', 'p-001', 'Product 001');
-insert into product (id, code, name) values ('p001', 'p-001', 'Product 001');
-insert into product (id, code, name) values ('p001', 'p-001', 'Product 001');
+insert into product (id, code, name) values ('p002', 'p-002', 'Product 002');
+insert into product (id, code, name) values ('p003', 'p-003', 'Product 003');
